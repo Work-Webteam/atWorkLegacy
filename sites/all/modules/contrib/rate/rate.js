5ea9c3e5c5d7fc407fa340f72214d9dd831b4842
twerdal <thayne.werdal@gov.bc.ca>
2016-10-20 23:01:06 +0000

    forgot I updated the rate module - put the original back in

--- sites/all/modules/contrib/rate/rate.js
+++ sites/all/modules/contrib/rate/rate.js
@@ -23,7 +23,7 @@
     // Invoke JavaScript hook.
     widget.trigger('eventBeforeRate', [data]);
 
-    $(".rate-info", widget).text(Drupal.t('Saving vote...'));
+    $(".rate-info", widget).text(Drupal.t('Saving...'));
 
     // Random number to prevent caching, see http://drupal.org/node/1042216#comment-4046618
     var random = Math.floor(Math.random() * 99999);
