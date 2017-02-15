db0c24ad9f9a4997182ee47da5fdc94b545b8e8a
Raj Persram <raj.persram@gov.bc.ca>
2017-02-15 23:57:47 +0000

    updated dock js but still needs work. Last commit of the day

--- sites/all/modules/custom/atwork_activity/js_atwork_activity_undock.js
+++ sites/all/modules/custom/atwork_activity/js_atwork_activity_undock.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     $('.jsNewActivityWindow').click(function() {
       // opens in new window to the far right of screen, on top and accounting for multiple monitors
       var customWindow = window.open(this.href, "customWindow", "width=300, height=850, top=0, left=9000").focus();
+
       $(function() {
         // waits for dom to load and then scrolls all the way to the right.
         setTimeout(function() {
