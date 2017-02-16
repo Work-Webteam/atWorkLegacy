c20043da50c0316924f757193a3e596d08cb9d4c
Raj Persram <raj.persram@gov.bc.ca>
2017-02-16 17:01:08 +0000

    third attempt at the focus for the pop. Put a delay timer on the focus

--- sites/all/modules/custom/atwork_activity/js_atwork_activity_undock.js
+++ sites/all/modules/custom/atwork_activity/js_atwork_activity_undock.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     $('.jsNewActivityWindow').click(function() {
       // opens in new window to the far right of screen, on top and accounting for multiple monitors
       var customWindow = window.open(this.href, "customWindow", "width=300, height=850, top=0, left=9000");
-      customWindow.focus();
+      setTimeout(function(){customWindow.focus();},1000);
       $(function() {
         // waits for dom to load and then scrolls all the way to the right.
         setTimeout(function() {
