f5d2ea5acb38920d968aed8c16ddfd511b760adb
twerdal <thayne.werdal@gov.bc.ca>
2017-02-18 20:13:38 +0000

    changing times on jQuery

--- sites/all/modules/custom/atwork_activity/js_atwork_activity_undock.js
+++ sites/all/modules/custom/atwork_activity/js_atwork_activity_undock.js
@@ -7,18 +7,18 @@
         .bind('click', function(){
           // opens in new window to the far right of screen, on top and accounting for multiple monitors
           var customWindow = window.open(this.href, "customWindow", "scrollbars=1, width=300, height=850, top=0, left=9000");
-          setTimeout(function(){window.blur();},1000);
-          setTimeout(function(){customWindow.focus();},1200);
-          setTimeout(function(){window.blur();},1250);
-          setTimeout(function(){customWindow.focus();},1350);
+          setTimeout(function(){window.blur();},3000);
+          setTimeout(function(){customWindow.focus();},3001);
+          setTimeout(function(){window.blur();},4000);
+          setTimeout(function(){customWindow.focus();},4001);
 
 
           if(window.focus){
-            setTimeout(customWindow.focus(), 2500);
+            setTimeout(customWindow.focus(), 5000);
           }
         // waits for dom to load and then scrolls all the way to the right.
         setTimeout(function() {
-          $(customWindow.document).scrollLeft(500);
+          $(customWindow.document).scrollLeft(2000);
         }, 10);
           return false;
         });
