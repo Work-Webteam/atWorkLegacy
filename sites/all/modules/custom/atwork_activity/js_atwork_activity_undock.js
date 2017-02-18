bbfb28326625b19f41a49da3e7e7e66b079f8a09
twerdal <thayne.werdal@gov.bc.ca>
2017-02-18 19:02:04 +0000

    More work on undock functioon - rewritten from ground up.

--- sites/all/modules/custom/atwork_activity/js_atwork_activity_undock.js
+++ sites/all/modules/custom/atwork_activity/js_atwork_activity_undock.js
@@ -1,20 +1,24 @@
 (function ($) {
-
-$(document).ready(function(){
-    $('.jsNewActivityWindow').click(function() {
-      // opens in new window to the far right of screen, on top and accounting for multiple monitors
-      var customWindow = window.open(this.href, "customWindow", "scrollbars=1, width=305, height=950, top=0, left=9000");
-      setTimeout(function(){window.blur();},500);
-      setTimeout(function(){customWindow.focus();},1000);
-
-      $(function() {
+  Drupal.behaviors.atwork_activity_undock = {
+    attach: function(context, settings) {
+      // Only run if the link exists in the current page load or fragment refresh.
+      $('.jsNewActivityWindow:not(.atwork-activity-processed)', context)
+        .addClass('atwork-activity-processed')
+        .bind('click', function(){
+          // opens in new window to the far right of screen, on top and accounting for multiple monitors
+          var customWindow = window.open(this.href, "customWindow", "scrollbars=1, width=300, height=850, top=0, left=9000");
+          setTimeout(function(){window.blur();},500);
+          setTimeout(function(){customWindow.focus();},1000);
+          if(window.focus){
+            setTimeout(customWindow.focus(), 1200);
+          }
         // waits for dom to load and then scrolls all the way to the right.
         setTimeout(function() {
           $(customWindow.document).scrollLeft(2000);
         }, 10);
-        customWindow.focus(); // focus, damn you
-      });
-      return false;
-    });
-  });
+          return false;
+        });
+    }
+  };
+
 }(jQuery));
