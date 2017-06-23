d3c303d7ae44aad887adb79c6a0804e67bce0fd3
BJenning <brendan.jennings@gov.bc.ca>
2017-06-23 18:55:20 +0000

    Modified atwork_search module to call js file when search button clicked, instead of using inline script.

---
+++ sites/all/modules/custom/atwork_search/js/atwork_search.js
@@ -0,0 +1,8 @@
+(function ($) {
+    $(document).ready(function(){
+        // Handle clicking on search button here.
+        $("#custom-search-blocks-form-1").("i").click(function(){
+            $("#custom-search-blocks-form-1").submit();
+        });
+    });
+})(jQuery);
