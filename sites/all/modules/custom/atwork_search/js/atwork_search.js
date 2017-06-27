880d4e5e4b90290f351db47c42533c0a9ea8e1bf
BJenning <brendan.jennings@gov.bc.ca>
2017-06-27 16:49:31 +0000

    Implemented js, sass for searching animation

--- sites/all/modules/custom/atwork_search/js/atwork_search.js
+++ sites/all/modules/custom/atwork_search/js/atwork_search.js
@@ -1,8 +1,27 @@
 (function ($) {
+
     $(document).ready(function(){
+
         // Handle clicking on search button here.
-        $("#custom-search-blocks-form-1").("i").click(function(){
-            $("#custom-search-blocks-form-1").submit();
+        $("#custom-search-blocks-form-1 #search-button").click(function(){
+            // Process search request
+            //$("#custom-search-blocks-form-1").submit();
+
+            setLoadingAnimation();            
         });
+
+        // Handle when users presses 'enter' to search
+        $('#edit-custom-search-blocks-form-1--2').keypress(function(event) {
+            if (event.which == 13) setLoadingAnimation();
+        });
+
+        var setLoadingAnimation = function () {
+            //console.log('1');
+            if($('#edit-custom-search-blocks-form-1--2').val()) {
+                $("#custom-search-blocks-form-1 #search-button").replaceWith('<div class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div><div class="rect6"></div></div>');
+            }
+        }
+
     });
+
 })(jQuery);
