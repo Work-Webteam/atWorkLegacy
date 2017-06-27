ad6dc7fdf71953406a9ac08674b20388586c840c
BJenning <brendan.jennings@gov.bc.ca>
2017-06-27 18:00:20 +0000

    Implemented search-busy animation for IE < 9.0

--- sites/all/modules/custom/atwork_search/js/atwork_search.js
+++ sites/all/modules/custom/atwork_search/js/atwork_search.js
@@ -16,10 +16,21 @@
         });
 
         var setLoadingAnimation = function () {
-            //console.log('1');
             if($('#edit-custom-search-blocks-form-1--2').val()) {
-                $("#custom-search-blocks-form-1 #search-button").replaceWith('<div class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div><div class="rect6"></div></div>');
+                if(checkIE() && checkIE() < 10) {
+                    //Set Loading animation for IE9.0 or lower
+                    $("#custom-search-blocks-form-1 #search-button").replaceWith('<div class="search-busy"></div>');
+                } else {
+                    // All other browsers
+                    $("#custom-search-blocks-form-1 #search-button").replaceWith('<div class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div><div class="rect6"></div></div>');
+                }
             }
+        }
+
+        var checkIE = function () {
+            // Return false if not IE, else return IE version number
+            var myNav = navigator.userAgent.toLowerCase();
+            return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
         }
 
     });
