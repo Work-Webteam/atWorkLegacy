ad768e2910da7013202d3204e4dacfedd71c8be8
twerdal <thayne.werdal@gov.bc.ca>
2017-09-26 16:53:05 +0000

    Updating origin to latest angular build.

--- sites/all/modules/custom/atwork_premiers_awards_angular/theme/atwork-premiers-awards-angular-component.tpl.php
+++ sites/all/modules/custom/atwork_premiers_awards_angular/theme/atwork-premiers-awards-angular-component.tpl.php
@@ -1,1 +1,6 @@
-<my-app>Loading</myapp>
+<h1> Premiers Awards Registration </h1>
+<?php dpm($variables); ?>
+<div class="prems-awards-register-questions">
+      <user [value]="<?php echo($variables['user']->field_gal_first_name['und'][0]['safe_value']); ?>"></user>
+    <my-app>Loading</myapp>
+</div>  
