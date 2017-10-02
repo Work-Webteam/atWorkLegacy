1a2bfd87930e2eef5b3276763c6e8499c1f62052
twerdal <thayne.werdal@gov.bc.ca>
2017-10-02 22:12:56 +0000

    Switching over to jQuery for now

--- sites/all/modules/custom/atwork_premiers_awards_angular/js/atwork_premiers_awards_angular.module.js
+++ sites/all/modules/custom/atwork_premiers_awards_angular/js/atwork_premiers_awards_angular.module.js
@@ -1,9 +1,15 @@
 (function(app) {
   app.AppModule =
     ng.core.NgModule({
-      imports: [ ng.platformBrowser.BrowserModule ],
-      declarations: [ app.AppComponent ],
-      bootstrap: [ app.AppComponent ]
+      imports: [ 
+        ng.platformBrowser.BrowserModule, 
+      ],
+      declarations: [ 
+        app.AppComponent 
+      ],
+      bootstrap: [ 
+        app.AppComponent 
+      ]
     })
     .Class({
       constructor: function() {}
