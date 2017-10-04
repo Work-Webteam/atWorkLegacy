ce5adbf1cb0297bcbc0295fa60d195286451797c
twerdal <thayne.werdal@gov.bc.ca>
2017-10-04 21:52:00 +0000

    json reply object generated.

--- sites/all/modules/custom/atwork_prem_awards/js_prem_awards_form.js
+++ sites/all/modules/custom/atwork_prem_awards/js_prem_awards_form.js
@@ -48,7 +48,8 @@
     // Now set up the dialog box    
     setDialog();
 
-    // TODO: Make this into an actual function
+    // TODO: Make this into an actual function. 
+    // Click handler for "edit" functionality
     $(".show-input-field").click(function (){
       // We need to account for the sid here - so only show fields within the specific fieldset.
       var currentSid = $(this).attr("sid");
@@ -59,6 +60,7 @@
       $(this).hide();
     });
 
+    // Click handler for "cancel" functionality
     $(".cancel-show-input-field").click(function (){
       // We need to account for the sid here - so only show fields within the specific fieldset
       var currentSid = $(this).attr("sid");
@@ -66,9 +68,14 @@
       $('.fieldset-prem-award-class-' + currentSid + ' .show-input-field').show();
       $('.fieldset-prem-award-class-' + currentSid + ' .save-form-field').hide();      
       $(this).hide();
-      
     });
 
+    // Click handler for save functionality
+    $(".save-form-field").click(function (){
+      // Get sid so we know which form we are saving
+      var currentSid = $(this).attr("sid");
+      var confirmation = saveUpdates(currentSid);
+    });
     form = dialog.find("form").on("submit", function(event) {
       event.preventDefault();
       redirectSubmit();
@@ -125,31 +132,48 @@
           // All input fields should be hidden on initial form launch
           formString += '<select name="webcast-' + index + '" value="' + value.webcast + '" class="prem-award-input">';
             // TODO: Get proper dates/times for this.  
-            formString += '<option value="Sept-17">VancouverIsland</option>';
-            formString += '<option value="Sept-22">Mainland</option>';
-            formString += '<option value="Sept-31">Outer Reaches</option>';
+            formString += '<option value="vancouverIsland">Vancouver Island</option>';
+            formString += '<option value="lowerMainland">Lower Mainland</option>';
+            formString += '<option value="interiorNorth">Outer Reaches</option>';
           formString += '</select>';
           // Number attending bundle
           formString += '<label for="attending-' + index + '">Number of Attendies: ' + value.numberAttending + '</label>';
-          formString += '<input type="text" name="attending-' + index + '" value="' + value.numberAttending + '" class="prem-award-input">';
+          formString += '<input type="text" name="attending-' + index + '" value="' + value.numberAttending + '" class="prem-award-input prem-award-attending">';
           // Name bundle
           formString += '<label for="name-' + index + '">Name: ' + value.name + '</label>';
-          formString += '<input type="text" name="name-' + index + '" value="' + value.name + '" class="prem-award-input">';
+          formString += '<input type="text" name="name-' + index + '" value="' + value.name + '" class="prem-award-input prem-award-name">';
           // Ministry bundle
           formString += '<label for="ministry-' + index + '">Ministry: ' + value.ministry + '</label>';
-          formString += '<input type="text" name="ministry-' + index + '" value="' + value.ministry + '" class="prem-award-input">';
+          formString += '<input type="text" name="ministry-' + index + '" value="' + value.ministry + '" class="prem-award-input prem-award-ministry">';
           // City bundle
           formString += '<label for="city-' + index + '">City: ' + value.city + '</label>';
-          formString += '<input type="text" name="city-' + index + '" value="' + value.city + '" class="prem-award-input">';
+          formString += '<input type="text" name="city-' + index + '" value="' + value.city + '" class="prem-award-input prem-award-city">';
           // The field edit and confirm button should be the only button available initially.
           formString += '<input type="button" class="show-input-field" sid="' + index + '" value="Edit">';
           // Cancel button in case they don't want to edit afterall
           formString += '<input type="button" class="cancel-show-input-field" sid="' + index + '" value="Cancel">';
           // Holder button to fire a function to post changes. This is hidden initially
-          formString += '<input type="button" class="save-form-field" id="save-form-field-' + index + '" value="save">';
+          formString += '<input type="button" class="save-form-field" sid="' + index + '" value="save">';
         formString += '</fieldset>';
       }
     });
     return formString;
+  }
+
+  function saveUpdates (sid){
+    jsonObj = [];
+    // TODO: something that builds array of changes and sends info to php
+    items = {};
+    items['webcast'] = $('.fieldset-prem-award-class-' + sid + ' select.prem-award-input').val();
+    items['attending'] = $('.fieldset-prem-award-class-' + sid + ' input.prem-award-input.prem-award-attending').val();
+    items['name'] = $('.fieldset-prem-award-class-' + sid + ' input.prem-award-input.prem-award-name').val();    
+    items['ministry'] = $('.fieldset-prem-award-class-' + sid + ' input.prem-award-input.prem-award-ministry').val();
+    items['city'] = $('.fieldset-prem-award-class-' + sid + ' input.prem-award-input.prem-award-city').val();
+    jsonObj.push(items);
+    console.log(jsonObj);
+    
+    // TODO: something that checks if save goes through
+    // TODO: If it goes through, change labels to reflect new values and give user a "saved" message - hidding inputs and buttons as needed
+    // TODO: If it does not go through, give use an "Error" message asking them to submit again.
   }
 })(jQuery);
