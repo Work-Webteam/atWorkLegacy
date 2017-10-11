597b10abf55c5d99e47cf5d6d954c19fdfb51353
twerdal <thayne.werdal@gov.bc.ca>
2017-10-11 20:11:44 +0000

    updates to submission. Forms now update, and new forms can be added.

--- sites/all/modules/custom/atwork_prem_awards/js_prem_awards_form.js
+++ sites/all/modules/custom/atwork_prem_awards/js_prem_awards_form.js
@@ -2,6 +2,7 @@
   Drupal.behaviors.prem_awards_form = {
     attach: function (context, settings) {
       // Fixing case where user name returns blank and creates a 500 (employee news case)
+      uid = (settings.atwork_prem_awards.uid);
       if((settings.atwork_prem_awards.user.length < 1) && uid == 0 ){
         settings.atwork_prem_awards.user = 'Employee News';
       }
@@ -29,6 +30,8 @@
     // Need to generate some HTML here and pop a model
     var user_name = items.applicant;
     var formString = '';
+    var uid = items.uid;
+    console.log(items);
 
     formString = buildString(items);
 
@@ -74,7 +77,7 @@
     $(".save-form-field").click(function (){
       // Get sid so we know which form we are saving
       var currentSid = $(this).attr("sid");
-      var confirmation = saveUpdates(currentSid);
+      var confirmation = saveUpdates(currentSid, uid);
     });
     form = dialog.find("form").on("submit", function(event) {
       event.preventDefault();
@@ -120,7 +123,7 @@
   function buildString(items) {
     var formString = '';
     $.each(items, function (index, value) {
-      if (index == 'applicant'){
+      if (index == 'applicant' || index == 'uid'){
         // skipping this one
         return;
       } else {
@@ -130,11 +133,34 @@
           // Using the label to show the current information that is entered. If this changes we should reflect that change here
           formString += '<label for="webcast-' + index + '">Webcast: ' + value.webcast + '</label>';
           // All input fields should be hidden on initial form launch
-          formString += '<select name="webcast-' + index + '" value="' + value.webcast + '" class="prem-award-input">';
+          // Use this to find default value
+          var castValue = "default";
+          if(value.webcast.indexOf('Island') > -1) {
+            castValue = 'vancouverIsland';
+          }
+          if(value.webcast.indexOf('Lower') > -1) {
+            castValue = 'lowerMainland';
+          }
+          if(value.webcast.indexOf('Interior') > -1) {
+            castValue = 'interiorNorth';
+          }
+          formString += '<select name="webcast-' + index + '" value="' + castValue + '" class="prem-award-input">';
             // TODO: Get proper dates/times for this.  
-            formString += '<option value="vancouverIsland">Vancouver Island</option>';
-            formString += '<option value="lowerMainland">Lower Mainland</option>';
-            formString += '<option value="interiorNorth">Outer Reaches</option>';
+            if(castValue == 'vancouverIsland'){
+              formString += '<option value="vancouverIsland" selected="selected">Vancouver Island</option>';
+            } else {
+              formString += '<option value="vancouverIsland">Vancouver Island</option>';              
+            }
+            if(castValue =='lowerMainland'){
+              formString += '<option value="lowerMainland" selected="selected">Lower Mainland</option>';
+            } else {
+              formString += '<option value="lowerMainland">Lower Mainland</option>';              
+            }
+            if(castValue == 'interiorNorth'){
+              formString += '<option value="interiorNorth" selected="selected">Interior / North</option>';
+            } else {
+              formString += '<option value="interiorNorth">Interior / North</option>';              
+            }
           formString += '</select>';
           // Number attending bundle
           formString += '<label for="attending-' + index + '">Number of Attendies: ' + value.numberAttending + '</label>';
@@ -157,23 +183,48 @@
         formString += '</fieldset>';
       }
     });
+
     return formString;
   }
 
-  function saveUpdates (sid){
-    jsonObj = [];
+  function saveUpdates (sid, uid){
     // TODO: something that builds array of changes and sends info to php
-    items = {};
-    items['webcast'] = $('.fieldset-prem-award-class-' + sid + ' select.prem-award-input').val();
-    items['attending'] = $('.fieldset-prem-award-class-' + sid + ' input.prem-award-input.prem-award-attending').val();
-    items['name'] = $('.fieldset-prem-award-class-' + sid + ' input.prem-award-input.prem-award-name').val();    
-    items['ministry'] = $('.fieldset-prem-award-class-' + sid + ' input.prem-award-input.prem-award-ministry').val();
-    items['city'] = $('.fieldset-prem-award-class-' + sid + ' input.prem-award-input.prem-award-city').val();
-    jsonObj.push(items);
-    console.log(jsonObj);
+    // Required fields and expected order from PHP: $webcast, $number_of_attendees, $name, $ministry, $city, $sid
+    var data = {};
+    data['webcast'] = $('.fieldset-prem-award-class-' + sid + ' select.prem-award-input').val();
+    data['attending'] = $('.fieldset-prem-award-class-' + sid + ' input.prem-award-input.prem-award-attending').val();
+    data['name'] = $('.fieldset-prem-award-class-' + sid + ' input.prem-award-input.prem-award-name').val();    
+    data['ministry'] = $('.fieldset-prem-award-class-' + sid + ' input.prem-award-input.prem-award-ministry').val();
+    data['city'] = $('.fieldset-prem-award-class-' + sid + ' input.prem-award-input.prem-award-city').val();
+    data['sid'] = sid;
+    data['uid'] = uid;
+    console.log(data);
+    $.ajax({
+      type: 'POST',
+      url: '/p-awards/submit',
+      dataType: 'json',
+      success: ajaxCompleted,
+      data: data,
+    });
     
+    // Now, pass through our json objec
+    //$.get('/p-awards/submit/' + jsonObj, null, submitForm);
     // TODO: something that checks if save goes through
-    // TODO: If it goes through, change labels to reflect new values and give user a "saved" message - hidding inputs and buttons as needed
+    //var submitForm = function (response) {
+    //  var result = $.parseJSON(response);
+      // Create a workable array of results
+      // Print it all out in the dom
+    //  console.log(result);
+      // Now set click handlers for updating fields
+    //  return false;
+    //};
+    // TODO: If it goes through, change labels to reflect new values and give user a "saved" message - hiding inputs and buttons as needed
     // TODO: If it does not go through, give use an "Error" message asking them to submit again.
+  }
+
+  function ajaxCompleted (returnData) {
+    console.log(returnData);
+    console.log("returned");
+    // Add some stuff to your DOM if this was successful - error and mark if it was not.
   }
 })(jQuery);
