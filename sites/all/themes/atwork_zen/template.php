d84401a2e1bc030381a36efb4bdc83707631cb63
Thayne Werdal <thayne.werdal@gov.bc.ca>
2018-01-05 22:32:40 +0000

    Removing old code from template.php

--- sites/all/themes/atwork_zen/template.php
+++ sites/all/themes/atwork_zen/template.php
@@ -38,16 +38,6 @@ function atwork_zen_preprocess_page(&$variables, $hook) {
       $variables['title_prefix'] = $snipet[0]['value'];
     }
   }
-  
-  // If this is an article page, add js file to correct sidebar image.
-  // This file does not exist in Repo - commenting out for now.
-  //if (isset($variables['node']->type) && !empty($variables['node']->type) && ($variables['node']->type == 'article')){
-  //	drupal_add_js('sites/all/themes/atwork_zen/js/article_sidebar_image.js');
-  //}
-  
-  if (isset($variables['node']->type) && !empty($variables['node']->type) && ($variables['node']->type == 'simplenews')){
-  	drupal_add_js('sites/all/themes/atwork_zen/js/newsletter.js');
-  }
 }
 
 
