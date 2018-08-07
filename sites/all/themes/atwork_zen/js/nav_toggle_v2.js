b758935e4c7841ad03557862ca564405edbd88c3
twerdal <thayne.werdal@gov.bc.ca>
2018-08-07 23:00:24 +0000

    Adjusted anchor tag scroll on page load, so that we see a scroll animation and leave the object we are looking for further down the page. Needs to be tested in IE

--- sites/all/themes/atwork_zen/js/nav_toggle_v2.js
+++ sites/all/themes/atwork_zen/js/nav_toggle_v2.js
@@ -95,6 +95,32 @@ Drupal.behaviors.atwork_quicklinks = {
 	} else {
 		$(".region-navigation").css("top", -2)
 	}
+/**
+ * This function removes previous action to scroll to anchor, and replaces it with an animated scroll that leaves the desired content lower on the page.
+ */
+	// to top right away
+if ( window.location.hash ) scroll(0,0);
+// void some browsers issue
+setTimeout( function() { scroll(0,0); }, 1);
+
+$(function() {
+
+    // your current click function
+    $('.scroll').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 200 + 'px'
+        }, 1000, 'swing');
+    });
+
+    // *only* if we have anchor on the url
+    if(window.location.hash) {
+        // smooth scroll to the anchor id
+        $('html, body').animate({
+            scrollTop: $(window.location.hash).offset().top - 200 + 'px'
+        }, 1000, 'swing');
+    }
+});
 	// Short term code to give a title on hover to banner div
 	//$('#header-box').prop('title', "First day of spring - March 20");
     //code ends
