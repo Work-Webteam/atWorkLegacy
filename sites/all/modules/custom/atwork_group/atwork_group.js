173bab078075dd859be685ea58121c3451bdbe7c
twerdal <thayne.werdal@gov.bc.ca>
2018-08-20 16:22:49 +0000

    Adding slider functionality to group admin list

--- sites/all/modules/custom/atwork_group/atwork_group.js
+++ sites/all/modules/custom/atwork_group/atwork_group.js
@@ -1,9 +1,18 @@
+/**
+ * Quick function to control the admin menu in groups - allows for accordian functionality.
+ */
 (function ($) {
-
   $( document ).ready(function(){
-    $("#admin-list-accordian").hide();
     $("#group-admin-block").click(function(){
-      $("#admin-list-accordian").slideToggle();
+        if($("#fieldset-admin-block").hasClass("collapsed")){
+          $("#admin-list-accordian").slideToggle();
+          $("#fieldset-admin-block").removeClass("collapsed");
+        } else {
+          // Wrap this in a function so we don't collapse before sliding
+          $("#admin-list-accordian").slideToggle(function(){
+            $("#fieldset-admin-block").addClass("collapsed");
+          });
+        }
     });
   });
 
