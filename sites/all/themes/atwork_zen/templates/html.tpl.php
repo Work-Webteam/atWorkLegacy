7936839b0026b072e023a6f29cd9d9929312fb48
twerdal <thayne.werdal@gov.bc.ca>
2020-05-07 18:58:46 +0000

    adding back in some shims

--- sites/all/themes/atwork_zen/templates/html.tpl.php
+++ sites/all/themes/atwork_zen/templates/html.tpl.php
@@ -10,6 +10,13 @@
 <head>
   <meta http-equiv="X-UA-Compatible" content="IE=EDGE" />
   <title><?php print $head_title; ?></title>
+  <!--[if !IE 7]><style type="text/css">#wrap {display:table;height:100%}</style><![endif]-->
+  <!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes; ?>><![endif]-->
+  <!--[if lte IE 6]><html class="lt-ie9 lt-ie8 lt-ie7" <?php print $html_attributes; ?>><![endif]-->
+  <!--[if (IE 7)&(!IEMobile)]><html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?>><![endif]-->
+  <!--[if IE 8]><html class="lt-ie9" <?php print $html_attributes; ?>><![endif]-->
+  <!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html <?php print $html_attributes . $rdf_namespaces; ?>><!--<![endif]-->
+  <!--[if lt IE 9]> <html class="old-ie" lang="en"> <!![endif]-->
   <?php print $head; ?>
 
   <?php if ($default_mobile_metatags): ?>
