1df5959d244636b1a4713c38a6c098c9db272eca
tvwerdal <thayne.werdal@gov.bc.ca>
2020-07-16 18:36:38 +0000

    Update to fix gallery pager

--- sites/all/modules/custom/atwork_images/js/js_atwork_images.js
+++ sites/all/modules/custom/atwork_images/js/js_atwork_images.js
@@ -7,14 +7,13 @@
       var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
       // Split into chunks
       var pathArray = window.location.pathname.split( '/' );
-      // Make sure we are on the correct page 
+      // Make sure we are on the correct page
       if($(pathArray).length >= 4 && pathArray[3] == 'gallery'){
         var match = RegExp('[?&]' + 'page' + '=([^&]*)').exec(window.location.search);
         // Capture our page number
         var galPg = match && decodeURIComponent(match[1].replace(/\+/g, ' '));
         if(galPg !== null) {
           // Add page query string on every image, so clicking will pass it in the URL
-          console.log("we have a page number");
           $('.views-field-field-image a').each(function() {
             var $this = $(this);
             href = $this.attr('href');
